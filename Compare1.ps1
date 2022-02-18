1815af2a400e0c779c6ccf8b3d2fdacca57f1629
matilda.hallden <matilda.hallden@toxic.se>
2022-02-18 11:55:19 +0100

    Updating format of expected data from queries

--- Compare1.ps1
+++ Compare1.ps1
@@ -1,6 +1,6 @@
-sqlcmd -S 127.0.0.1 -d AdventureWorksLT2019 -U sa -P "yourStrong(!)Password" -i SQLQuery1.sql -o StudentAns1.txt -W -h -1 -m 1
+sqlcmd -S 127.0.0.1 -d AdventureWorksLT2019 -U sa -P "yourStrong(!)Password" -i /test-helpers/SQLQuery1.sql -o StudentAns1.txt -W -h -1 -m 1
 $studentOutput = Get-Content -Path StudentAns1.txt -Raw
-$expectedOutput =Get-Content -Path /home/runner/work/labb4/labb4/testdir/ExpectedOutput1.txt -Raw
+$expectedOutput =Get-Content -Path /test-helpers/ExpectedOutput1.txt -Raw
 $studentOutput -eq $expectedOutput
 $studentOutput 
 $expectedOutput

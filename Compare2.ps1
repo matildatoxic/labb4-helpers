0bf92000107152b891e88959c04086a0a0388709
matilda.hallden <matilda.hallden@toxic.se>
2022-02-18 12:07:44 +0100

    Adding correct file path to expected output

--- Compare2.ps1
+++ Compare2.ps1
@@ -1,4 +1,4 @@
 sqlcmd -S 127.0.0.1 -d AdventureWorksLT2019 -U sa -P "yourStrong(!)Password" -i SQLQuery2.sql -o StudentAns2.txt -W -h -1 -m 1
 $studentOutput = Get-Content -Path StudentAns2.txt -Raw
-$expectedOutput =Get-Content -Path /home/runner/work/labb4/labb4/testdir/ExpectedOutput2.txt -Raw
+$expectedOutput =Get-Content -Path ./test-helpers/ExpectedOutput2.txt -Raw
 $studentOutput -eq $expectedOutput

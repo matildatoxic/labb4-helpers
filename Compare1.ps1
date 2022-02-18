606ec609c84c702ba88449b1cff6b28f2d523190
matilda.hallden <matilda.hallden@toxic.se>
2022-02-18 10:56:55 +0100

    Printing output to check for errors

--- Compare1.ps1
+++ Compare1.ps1
@@ -2,3 +2,5 @@ sqlcmd -S 127.0.0.1 -d AdventureWorksLT2019 -U sa -P "yourStrong(!)Password" -i 
 $studentOutput = Get-Content -Path StudentAns1.txt -Raw
 $expectedOutput =Get-Content -Path /home/runner/work/labb4/labb4/testdir/ExpectedOutput1.txt -Raw
 $studentOutput -eq $expectedOutput
+$studentOutput 
+$expectedOutput
